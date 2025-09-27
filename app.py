62626d51bc5b1ac5fbe8c01b67a58e8a3d970d55
lightsail_ubuntu <anugraha.londhe@gmail.com>
2025-09-27 03:39:08 +0000

    changed the main port to 5000 to avoid conflict with the main domain

--- app.py
+++ app.py
@@ -72,5 +72,6 @@ def logout():
 def health():
     return "OK", 200
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
